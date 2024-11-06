85387d81c507abb0b020c6792ce26639a5a57c28
maxi-q <maximkabust@gmail.com>
2024-11-06 17:04:19 +0300

    idk

---
+++ src/store/account.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+interface Account {
+  user_id: string,
+  group_id: string,
+  auth: string,
+  context: string
+}
+
+interface CountStore {
+  account: Account;
+  setAccount: () => void;
+}
+
+const useCountStore = create<CountStore>((set) => ({
+  account: {
+    user_id: '',
+    group_id: '',
+    auth: '',
+    context: ''
+  },
+  setAccount: () => set((state) => (state))
+}));
+
+export default useCountStore
+
