c750d0058937b0f820a9f122d2b1752a16941e09
maxi-q <maximkabust@gmail.com>
2024-12-15 16:34:20 +0300

    .

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -51,7 +51,7 @@ export const Page = () => {
               type
 						},
             description: 'description',
-            command_title: 'command_title'
+            command_title: 'command_title 2',
 					},
 					success: true
 				},
