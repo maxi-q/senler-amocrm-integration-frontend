c022ee76950e2a9e18b5a3703515abe404da52a0
maxi-q <maximkabust@gmail.com>
2025-02-11 17:04:10 +0300

    etst

--- src/pages/DataManagement/components/KeyValueInput/index.tsx
+++ src/pages/DataManagement/components/KeyValueInput/index.tsx
@@ -15,22 +15,23 @@ interface IEditableTableProps {
   senlerFields: ISenlerField[]
 }
 
-const EditableTable = ({ data = [], changeData, amoCRMFields, senlerFields }: IEditableTableProps) => {
+const EditableTable = ({ data = [] }: IEditableTableProps) => {
   const [currentData, setCurrentData] = useState<IDataRow[]>(data);
 
   useEffect(() => {
     setCurrentData(data)
     console.log('data112 ', data)
+    console.log(currentData)
   }, [data])
 
-  const handleValueChange = (rowIndex: number, key: keyof IDataRow, newValue: string) => {
-    setCurrentData((prevData) => {
-      const updatedData = [...prevData];
-      updatedData[rowIndex] = { ...updatedData[rowIndex], [+key]: newValue };
-      changeData(updatedData);
-      return updatedData;
-    });
-  };
+  // const handleValueChange = (rowIndex: number, key: keyof IDataRow, newValue: string) => {
+  //   setCurrentData((prevData) => {
+  //     const updatedData = [...prevData];
+  //     updatedData[rowIndex] = { ...updatedData[rowIndex], [+key]: newValue };
+  //     changeData(updatedData);
+  //     return updatedData;
+  //   });
+  // };
 
   const handleAddRow = () => {
     setCurrentData((prevData) => [
@@ -39,9 +40,9 @@ const EditableTable = ({ data = [], changeData, amoCRMFields, senlerFields }: IE
     ]);
   };
 
-  const handleDeleteRow = (rowIndex: number) => {
-    setCurrentData((prevData) => prevData.filter((_, index) => index !== rowIndex));
-  };
+  // const handleDeleteRow = (rowIndex: number) => {
+  //   setCurrentData((prevData) => prevData.filter((_, index) => index !== rowIndex));
+  // };
 
   return (
     <div style={{ width: '100%', maxWidth: '800px', margin: 'auto' }}>
