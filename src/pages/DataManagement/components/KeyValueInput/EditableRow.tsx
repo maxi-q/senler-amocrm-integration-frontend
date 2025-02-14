f3db2303f5b5cba7b4a4b8629255a9d570f07102
maxi-q <maximkabust@gmail.com>
2025-02-14 21:08:39 +0300

    debug

--- src/pages/DataManagement/components/KeyValueInput/EditableRow.tsx
+++ src/pages/DataManagement/components/KeyValueInput/EditableRow.tsx
@@ -16,7 +16,7 @@ interface IEditableRowProps {
 const EditableRow = memo(({ row, rowIndex, onValueChange, onDelete, toFields = [], fromFields = [] }: IEditableRowProps) => {
   const label = (field: IAmoCRMField | ISenlerField) => field instanceof IAmoCRMField ? field.name : field.text
 
-  console.log(toFields && label(toFields[0]), toFields[0] instanceof IAmoCRMField)
+  console.log(toFields[0] && label(toFields[0]), toFields[0] instanceof IAmoCRMField)
 
   const amoCRMFieldsOptions = toFields?.map(field => ({ value: field.id, label: label(field) }))
   const senlerFieldsOptions = fromFields?.map(field => ({ value: field.id, label: label(field) }))
