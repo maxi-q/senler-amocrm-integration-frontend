6c106d49975c0d72111e4162564459891cd854ab
maxi-q <maximkabust@gmail.com>
2025-03-03 09:50:51 +0300

    style

--- src/pages/DataManagement/components/KeyValueInput/EditableRow.tsx
+++ src/pages/DataManagement/components/KeyValueInput/EditableRow.tsx
@@ -4,43 +4,51 @@ import MySelect from './SelectField';
 import { IAmoCRMField } from '@/api/Backend/fields/fields.dto';
 import { ISenlerField } from '../../modules/SendDataToAmoCrm/SendDataToAmoCrm.dto';
 
+import styles from './styles.module.css'
+
 interface IEditableRowProps {
   row: IDataRow;
   rowIndex: number;
   onValueChange: (rowIndex: number, key: keyof IDataRow, newValue: string) => void;
   onDelete: (rowIndex: number) => void;
-  toFields?: IAmoCRMField[] | ISenlerField[],
-  fromFields?: IAmoCRMField[] | ISenlerField[],
+  toFields?: IAmoCRMField[] | ISenlerField[];
+  fromFields?: IAmoCRMField[] | ISenlerField[];
 }
 
 const EditableRow = memo(({ row, rowIndex, onValueChange, onDelete, toFields = [], fromFields = [] }: IEditableRowProps) => {
-  const label = (field: IAmoCRMField | ISenlerField) => field instanceof ISenlerField ? field.text : field.name
+  const getLabel = (field: IAmoCRMField | ISenlerField): string => {
+    return field instanceof ISenlerField ? field.text : field.name;
+  };
 
-  console.log(toFields, toFields[0] && label(toFields[0]), toFields[0] instanceof ISenlerField)
-  console.log(fromFields, fromFields[0] && label(fromFields[0]), fromFields[0] instanceof ISenlerField)
-
-  const amoCRMFieldsOptions = toFields?.map(field => ({ value: field.id, label: label(field) }))
-  const senlerFieldsOptions = fromFields?.map(field => ({ value: field.id, label: label(field) }))
+  const amoCRMFieldsOptions = toFields.map(field => ({ value: field.id, label: getLabel(field) }));
+  const senlerFieldsOptions = fromFields.map(field => ({ value: field.id, label: getLabel(field) }));
 
   return (
     <tr>
       <td>
-        <MySelect value={row.from} rowIndex={rowIndex} onValueChange={onValueChange} options={senlerFieldsOptions} label='Senler' sourceKey='from'/>
+        <MySelect
+          value={row.from}
+          rowIndex={rowIndex}
+          onValueChange={onValueChange}
+          options={senlerFieldsOptions}
+          label="Senler"
+          sourceKey="from"
+        />
       </td>
       <td>
-        <MySelect value={row.to} rowIndex={rowIndex} onValueChange={onValueChange} options={amoCRMFieldsOptions} label='AmoCRM' sourceKey='to'/>
+        <MySelect
+          value={row.to}
+          rowIndex={rowIndex}
+          onValueChange={onValueChange}
+          options={amoCRMFieldsOptions}
+          label="AmoCRM"
+          sourceKey="to"
+        />
       </td>
       <td>
         <button
           onClick={() => onDelete(rowIndex)}
-          style={{
-            padding: '4px 8px',
-            backgroundColor: '#f44336',
-            color: 'white',
-            border: 'none',
-            borderRadius: '4px',
-            cursor: 'pointer',
-          }}
+          className={styles["delete-button"]}
           title="Удалить строку"
         >
           X
