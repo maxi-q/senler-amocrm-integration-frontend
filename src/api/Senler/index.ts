1174e8340abb70e31bda9debad2d69873b6296d7
maxi-q <maximkabust@gmail.com>
2025-06-06 16:25:12 +0300

    fix

--- src/api/Senler/index.ts
+++ src/api/Senler/index.ts
@@ -34,8 +34,7 @@ export const OAuth2token = async ({client_id, client_secret, redirect_uri, code}
           client_id: client_id,
           client_secret: client_secret,
           redirect_uri: redirect_uri,
-          code: code,
-          state: Date.now()
+          code: code
         },
       }
     )
