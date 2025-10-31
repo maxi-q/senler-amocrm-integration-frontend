411c0bf4cb0b8c41b983b5c3c914cb45deb0bc3f
maxi-q <maximkabust@gmail.com>
2025-10-31 17:05:16 +0300

    PLG-112

--- src/pages/list_integrations/Register/index.tsx
+++ src/pages/list_integrations/Register/index.tsx
@@ -18,6 +18,8 @@ export const Register = () => {
           <div style={{ marginTop: 16, marginBottom: 16 }}>
             Интеграция позволит вам использовать AmoCRM в своих чат-ботах Senler.<br /><br />
             После установки в конструкторе чат-ботов добавляется шаг, позволяющий передавать подписчиков с переменными в AmoCRM и наоборот.
+            <br /><br />
+            Подробнее в <a href="https://help.senler.ru/senler/kanaly/vkontakte/integracii/integraciya-s-amocrm" target="_blank" rel="noopener noreferrer" style={{ color: '#0066cc', textDecoration: 'underline' }}>документации</a>.
           </div>
         </>
       }
