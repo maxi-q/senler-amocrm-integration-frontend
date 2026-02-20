daccf328194766f0a14fcd1d736ab04740d0a1d0
maxi-q <maximkabust@gmail.com>
2026-02-20 18:01:13 +0300

    link

--- src/pages/Bot_step/DataManagement/components/KeyValueInput/index.tsx
+++ src/pages/Bot_step/DataManagement/components/KeyValueInput/index.tsx
@@ -63,7 +63,7 @@ const EditableTable = memo(({ data, changeData, toFields, fromFields, type='senl
       <div className='flex w-full justify-between items-center'>
         <div className='text-start'>
           <h3>Передача значений</h3>
-          <p className="mt-1 text-[0.7rem]">Максимум 2000 символов, подробнее в (документации)</p>
+          <p className="mt-1 text-[0.7rem]">Максимум 2000 символов ⚠️ подробнее в <a className='text-blue-700 hover:text-blue-800 underline' href='https://help.senler.ru/senler/kanaly/vkontakte/integracii/integraciya-s-amocrm'>документации</a></p>
         </div>
 
         <button
